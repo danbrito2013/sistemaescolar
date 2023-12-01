f52e2388c375ae5c9081554e25d1141282e479a9
danbrito2013 <126628569+danbrito2013@users.noreply.github.com>
2023-11-30 21:39:19 -0300

    Update RemoverAluno.java

--- src/orm/actions/RemoverAluno.java
+++ src/orm/actions/RemoverAluno.java
@@ -3,15 +3,21 @@ package orm.actions;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
 import orm.modelo.Aluno;
+
+import java.util.Scanner;
 
 public class RemoverAluno {
     public static void main(String[] args) {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("sistemaescolar");
         EntityManager manager = factory.createEntityManager();
 
-        Aluno aluno = manager.find(Aluno.class, 2L);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o ID do aluno a ser removido: ");
+        long alunoId = scanner.nextLong();
+
+        Aluno aluno = manager.find(Aluno.class, alunoId);
 
         if (aluno != null) {
             manager.getTransaction().begin();
@@ -22,10 +28,11 @@ public class RemoverAluno {
 
             System.out.println("Aluno removido com sucesso.");
         } else {
-            System.out.println("Aluno não encontrado.");
+            System.out.println("Aluno não encontrado para o ID informado.");
         }
 
         manager.close();
         factory.close();
+        scanner.close();
     }
 }
