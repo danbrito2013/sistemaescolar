118913b4efde5e2259999ceda853d7c290990caf
danbrito2013 <126628569+danbrito2013@users.noreply.github.com>
2023-11-30 21:21:35 -0300

    alterações

--- src/orm/actions/InserirAluno.java
+++ src/orm/actions/InserirAluno.java
@@ -1,64 +1,57 @@
 package orm.actions;
 
-import javax.persistence.*;
+import java.util.Scanner;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import orm.modelo.Aluno;
 
-import java.util.List;
-import java.util.Scanner;
-
 public class InserirAluno {
-    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("sistemaescolar");
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Obtenha dados do usuário
-        System.out.println("Informe o nome do aluno: ");
+        System.out.print("Digite o nome do aluno: ");
         String nome = scanner.nextLine();
 
-        // Continue para os outros atributos
-
-        // Crie uma instância de Aluno com os dados do usuário
-        Aluno aluno = new Aluno();
-        aluno.setNome(nome);
-
-        System.out.println("Informe o e-mail do aluno: ");
+        System.out.print("Digite o e-mail do aluno: ");
         String email = scanner.nextLine();
-        aluno.setEmail(email);
 
-        System.out.println("Informe o CPF do aluno: ");
+        System.out.print("Digite o CPF do aluno: ");
         String cpf = scanner.nextLine();
-        aluno.setCpf(cpf);
 
-        System.out.println("Informe a naturalidade do aluno: ");
+        System.out.print("Digite a data de nascimento do aluno (formato: dd/MM/yyyy): ");
+        String dataNascimento = scanner.nextLine();
+
+        System.out.print("Digite a naturalidade do aluno: ");
         String naturalidade = scanner.nextLine();
-        aluno.setNaturalidade(naturalidade);
 
-        System.out.println("Informe o endereço do aluno: ");
+        System.out.print("Digite o endereço do aluno: ");
         String endereco = scanner.nextLine();
-        aluno.setEndereco(endereco);
 
-        // Inserir aluno no banco de dados
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(aluno);
-        em.getTransaction().commit();
-        em.close();
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("sistemaescolar");
+        EntityManager manager = factory.createEntityManager();
 
-        // Listar alunos do banco de dados
-        EntityManager emListar = emf.createEntityManager();
-        List<Aluno> alunos = emListar.createQuery("SELECT a FROM Aluno a", Aluno.class).getResultList();
-        emListar.close();
+        try {
+            manager.getTransaction().begin();
 
-        System.out.println("Lista de Alunos:");
-        for (Aluno a : alunos) {
-            System.out.println("Nome: " + a.getNome());
-            System.out.println("E-mail: " + a.getEmail());
-            System.out.println("CPF: " + a.getCpf());
-            System.out.println("Naturalidade: " + a.getNaturalidade());
-            System.out.println("Endereço: " + a.getEndereco());
+            Aluno aluno = new Aluno(nome, email, cpf, dataNascimento, naturalidade, endereco);
+
+            manager.persist(aluno);
+
+            manager.getTransaction().commit();
+
+            System.out.println("Aluno inserido com sucesso!");
+
+        } catch (Exception e) {
+            if (manager.getTransaction().isActive()) {
+                manager.getTransaction().rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            manager.close();
+            factory.close();
+            scanner.close();
         }
-
-        emf.close();
     }
 }
--- src/orm/actions/RemoverAluno.java
+++ src/orm/actions/RemoverAluno.java
@@ -3,21 +3,15 @@ package orm.actions;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import orm.modelo.Aluno;
 
-import java.util.Scanner;
+import orm.modelo.Aluno;
 
 public class RemoverAluno {
     public static void main(String[] args) {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("sistemaescolar");
         EntityManager manager = factory.createEntityManager();
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Informe o ID do aluno a ser removido: ");
-        long alunoId = scanner.nextLong();
-
-        Aluno aluno = manager.find(Aluno.class, alunoId);
+        Aluno aluno = manager.find(Aluno.class, 2L);
 
         if (aluno != null) {
             manager.getTransaction().begin();
@@ -28,11 +22,10 @@ public class RemoverAluno {
 
             System.out.println("Aluno removido com sucesso.");
         } else {
-            System.out.println("Aluno não encontrado para o ID informado.");
+            System.out.println("Aluno não encontrado.");
         }
 
         manager.close();
         factory.close();
-        scanner.close();
     }
 }
